ee55f67205a88f9f75cfb6a72c8e3b946876baf1
Jeff Pierce <drjpierce@gmail.com>
2013-05-29 15:10:16 -0700

    Added new CrimeLab class, CrimeList fragment and activity, and refactored the activity class structure.

---
+++ CriminalIntent/src/com/samsung/sra/tutorial/criminalintent/CrimeLab.java
@@ -0,0 +1,45 @@
+package com.samsung.sra.tutorial.criminalintent;
+
+import java.util.ArrayList;
+import java.util.UUID;
+import android.content.Context;
+
+public class CrimeLab {
+	private static CrimeLab sCrimeLab;
+	private Context mAppContext;
+	private ArrayList<Crime> mCrimes;
+	
+	private CrimeLab(Context appContext) {
+		mAppContext = appContext;
+		mCrimes = new ArrayList<Crime>();
+		
+		// Temporary generation of placeholder crimes
+		for (int i = 0; i < 100; i++) {
+			Crime c = new Crime();
+			c.setTitle("Crime #" + i);
+			c.setSolved( i % 2 == 0); // Every other crime is solved
+			mCrimes.add(c);
+		}
+	}
+	
+	public static CrimeLab get(Context c) {
+		if (sCrimeLab == null) {
+			sCrimeLab = new CrimeLab(c.getApplicationContext());
+		}
+		
+		return sCrimeLab;
+	}
+	
+	public ArrayList<Crime> getCrimes() {
+		return mCrimes;
+	}
+	
+	public Crime getCrime(UUID id) {
+		for (Crime c : mCrimes) {
+			if (c.getId().equals(id))
+				return c;
+		}
+		
+		return null;
+	}
+}
--- CriminalIntent/src/com/samsung/sra/tutorial/criminalintent/CrimeListActivity.java
+++ CriminalIntent/src/com/samsung/sra/tutorial/criminalintent/CrimeListActivity.java
@@ -2,12 +2,11 @@ package com.samsung.sra.tutorial.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
+public class CrimeListActivity extends SingleFragmentActivity {
 
 	@Override
 	protected Fragment createFragment() {
-		return new CrimeFragment();
+		return new CrimeListFragment();
 	}
 
-	
 }
