f2e96b0a9c69c8f7ed5462208e801e84aa75d4e5
Jeff Pierce <drjpierce@gmail.com>
2013-06-06 12:15:05 -0700

    Adds audio playback to the app

---
+++ HelloMoon/src/com/samsung/sra/hellomoon/AudioPlayer.java
@@ -0,0 +1,35 @@
+package com.samsung.sra.hellomoon;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+
+public class AudioPlayer {
+	private MediaPlayer mPlayer;
+	
+	public void stop() {
+		if (mPlayer != null) {
+			mPlayer.release();
+			mPlayer = null;
+		}
+	}
+	
+	public void play(Context c) {
+		if (mPlayer == null) {
+			mPlayer = MediaPlayer.create(c, R.raw.one_small_step);
+			mPlayer.setOnCompletionListener(new OnCompletionListener() {
+				
+				@Override
+				public void onCompletion(MediaPlayer mp) {
+					stop();
+				}
+			});
+		}
+		
+		mPlayer.start();
+	}
+	
+	public void pause() {
+		mPlayer.pause();
+	}
+}
--- HelloMoon/src/com/samsung/sra/hellomoon/HelloMoonFragment.java
+++ HelloMoon/src/com/samsung/sra/hellomoon/HelloMoonFragment.java
@@ -8,16 +8,51 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 public class HelloMoonFragment extends Fragment {
-	private Button mPlayButton;
+	private AudioPlayer mPlayer = new AudioPlayer();
+	private Button mPlayPauseButton;
+	private Boolean mPaused = true;
 	private Button mStopButton;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_hello_moon, parent, false);
 		
-		mPlayButton = (Button) v.findViewById(R.id.hellomoon_playButton);
+		mPlayPauseButton = (Button) v.findViewById(R.id.hellomoon_playPauseButton);
+		mPlayPauseButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if (mPaused) {
+					mPlayPauseButton.setText(R.string.hellomoon_pause);
+					mPlayer.play(getActivity());
+					mPaused = false;
+				} else {
+					mPlayPauseButton.setText(R.string.hellomoon_play);
+					mPlayer.pause();
+					mPaused = true;
+				}
+					
+			}
+		});
+		
 		mStopButton = (Button) v.findViewById(R.id.hellomoon_stopButton);
+		mStopButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				mPlayer.stop();
+				mPlayPauseButton.setText(R.string.hellomoon_play);
+				mPaused = true;
+			}
+		});
+		
 		
 		return v;
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		mPlayer.stop();
 	}
 }
