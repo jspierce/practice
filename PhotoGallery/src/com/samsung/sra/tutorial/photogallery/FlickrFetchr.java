37295a18197480bade330b35321a6fe9586ec6c6
Jeff Pierce <drjpierce@gmail.com>
2014-03-25 14:01:01 -0700

    Building out recent photo retrieval from Flickr.

--- PhotoGallery/src/com/samsung/sra/tutorial/photogallery/FlickrFetchr.java
+++ PhotoGallery/src/com/samsung/sra/tutorial/photogallery/FlickrFetchr.java
@@ -6,34 +6,57 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class FlickrFetchr {
+import android.net.Uri;
+import android.util.Log;
 
-		byte[] getUrlBytes(String urlSpec) throws IOException {
-			URL url = new URL(urlSpec);
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			
-			try {
-				ByteArrayOutputStream out = new ByteArrayOutputStream();
-				InputStream in = connection.getInputStream();
-				
-				if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-					return null;
-				}
-				
-				int bytesRead = 0;
-				byte[] buffer = new byte[1024];
-				while ((bytesRead = in.read(buffer)) > 0) {
-					out.write(buffer, 0, bytesRead);
-				}
-				
-				out.close();
-				return out.toByteArray();
-			} finally {
-				connection.disconnect();
-			}
-		}
+public class FlickrFetchr {
+	private static final String TAG = "FlickrFetchr";
+	
+	private static final String ENDPOINT = "http://api.flickr.com/services/rest/";
+	private static final String AppProInt_Code = "<code here>";
+	private static final String METHOD_GET_RECENT = "flickr.photos.getRecent";
+	private static final String PARAM_EXTRAS = "extras";
+	private static final String EXTRA_SMALL_URL = "url_s";
+	
+	byte[] getUrlBytes(String urlSpec) throws IOException {
+		URL url = new URL(urlSpec);
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 		
-		public String getUrl(String urlSpec) throws IOException {
-			return new String(getUrlBytes(urlSpec));
+		try {
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			InputStream in = connection.getInputStream();
+				
+			if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+				return null;
+			}
+				
+			int bytesRead = 0;
+			byte[] buffer = new byte[1024];
+			while ((bytesRead = in.read(buffer)) > 0) {
+				out.write(buffer, 0, bytesRead);
+			}
+				
+			out.close();
+			return out.toByteArray();
+		} finally {
+			connection.disconnect();
 		}
+	}
+		
+	public String getUrl(String urlSpec) throws IOException {
+		return new String(getUrlBytes(urlSpec));
+	}
+	
+	public void fetchItems() {
+		try {
+			String url = Uri.parse(ENDPOINT).buildUpon().appendQueryParameter("method", METHOD_GET_RECENT)
+														.appendQueryParameter("api_key", AppProInt_Code)
+														.appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
+														.build().toString();
+			String xmlString = getUrl(url);
+			Log.i(TAG, "Received xml: " + xmlString);
+		} catch (IOException ioe) {
+			Log.e(TAG, "Failed to fetch items", ioe);
+		}
+	}
 }
--- PhotoGallery/src/com/samsung/sra/tutorial/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/src/com/samsung/sra/tutorial/photogallery/PhotoGalleryFragment.java
@@ -1,11 +1,8 @@
 package com.samsung.sra.tutorial.photogallery;
 
-import java.io.IOException;
-
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,12 +31,7 @@ public class PhotoGalleryFragment extends Fragment {
 	private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
 		@Override
 		protected Void doInBackground(Void... params) {
-			try {
-				String result = new FlickrFetchr().getUrl("http://www.google.com");
-				Log.i(TAG, "Fetched contents of URL: " + result);
-			} catch (IOException ioe) {
-				Log.e(TAG, "Failed to fetch URL: ", ioe);
-			}
+			new FlickrFetchr().fetchItems();
 			
 			return null;
 		}
