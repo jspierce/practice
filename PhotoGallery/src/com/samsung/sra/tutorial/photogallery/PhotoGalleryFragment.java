8efa24cc988e772b6a0b7684382c3b4c525cd365
Jeff Pierce <drjpierce@gmail.com>
2014-03-25 11:42:53 -0700

    Adds URL fetching to PhotoGallery.

--- PhotoGallery/src/com/samsung/sra/tutorial/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/src/com/samsung/sra/tutorial/photogallery/PhotoGalleryFragment.java
@@ -1,13 +1,18 @@
 package com.samsung.sra.tutorial.photogallery;
 
+import java.io.IOException;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
 public class PhotoGalleryFragment extends Fragment {
+	private static final String TAG = "PhotoGalleryFragment";
 	GridView mGridView;
 	
 	@Override
@@ -15,6 +20,7 @@ public class PhotoGalleryFragment extends Fragment {
 		super.onCreate(savedInstanceState);
 		
 		setRetainInstance(true);
+		new FetchItemsTask().execute();
 	}
 	
 	@Override
@@ -23,5 +29,19 @@ public class PhotoGalleryFragment extends Fragment {
 		
 		mGridView = (GridView) v.findViewById(R.id.gridView);
 		return v;
+	}
+	
+	private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+				String result = new FlickrFetchr().getUrl("http://www.google.com");
+				Log.i(TAG, "Fetched contents of URL: " + result);
+			} catch (IOException ioe) {
+				Log.e(TAG, "Failed to fetch URL: ", ioe);
+			}
+			
+			return null;
+		}
 	}
 }
