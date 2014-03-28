7b5c47add196834c987cb526e21697611be0ae1a
Jeff Pierce <drjpierce@gmail.com>
2014-03-28 15:12:39 -0700

    Updates RunTracker to use the test location data provider if it is active.

--- RunTracker/src/com/samsung/sra/tutorial/runtracker/RunManager.java
+++ RunTracker/src/com/samsung/sra/tutorial/runtracker/RunManager.java
@@ -3,12 +3,16 @@ package com.samsung.sra.tutorial.runtracker;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
 import android.location.LocationManager;
+import android.util.Log;
 
 public class RunManager {
-	//private static final String TAG = "RunManager";
+	private static final String TAG = "RunManager";
 	
 	public static final String ACTION_LOCATION = "com.samsung.sra.tutorial.runtracker.ACTION_LOCATION";
+	
+	private static final String TEST_PROVIDER = "TEST_PROVIDER";
 	
 	private static final int MINIMUM_TIME_INTERVAL = 0; // milliseconds
 	private static final int MINIMUM_DISTANCE_INTERVAL = 0; // meters
@@ -43,6 +47,20 @@ public class RunManager {
 	public void startLocationUpdates() {
 		String provider = LocationManager.GPS_PROVIDER;
 		
+		// If we have the test provider installed and it's enabled, use it
+		if (mLocationManager.getProvider(TEST_PROVIDER) != null && mLocationManager.isProviderEnabled(TEST_PROVIDER)) {
+			provider = TEST_PROVIDER;
+		}
+		Log.d(TAG, "Using provider " + provider);
+		
+		// Get the last known location (if there is one) and broadcast it
+		Location lastKnown = mLocationManager.getLastKnownLocation(provider);
+		if (lastKnown != null) {
+			// Reset the time to now
+			lastKnown.setTime(System.currentTimeMillis());
+			broadcastLocation(lastKnown);
+		}
+		
 		// Start updates from the location manager
 		PendingIntent pi = getLocationPendingIntent(true);
 		mLocationManager.requestLocationUpdates(provider, MINIMUM_TIME_INTERVAL, MINIMUM_DISTANCE_INTERVAL, pi);
@@ -55,6 +73,13 @@ public class RunManager {
 			pi.cancel();
 		}
 	}
+	
+	private void broadcastLocation(Location location) {
+		Intent broadcast = new Intent(ACTION_LOCATION);
+		broadcast.putExtra(LocationManager.KEY_LOCATION_CHANGED, location);
+		mAppContext.sendBroadcast(broadcast);
+	}
+	
 	
 	public boolean isTrackingRun() {
 		return getLocationPendingIntent(false) != null;
