b2d6305b8f35f0a1dc40da3015dfcf29aeb29977
Jeff Pierce <drjpierce@gmail.com>
2014-03-28 11:55:06 -0700

    Warning patrol.

--- DragAndDraw/src/com/samsung/sra/tutorial/draganddraw/BoxDrawingView.java
+++ DragAndDraw/src/com/samsung/sra/tutorial/draganddraw/BoxDrawingView.java
@@ -2,7 +2,6 @@ package com.samsung.sra.tutorial.draganddraw;
 
 import java.util.ArrayList;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
